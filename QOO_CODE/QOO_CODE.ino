897d137fa9a7724c1b1fb5e325fc871dc6ec65ac
羽山 <linainverseshadow@gmail.com>
2021-08-16 00:48:17 +0800

    先簽一下

--- QOO_CODE/QOO_CODE.ino
+++ QOO_CODE/QOO_CODE.ino
@@ -1,14 +1,97 @@
-//田同學  2021/8/12 夜
-unsigned long p0_t = 0;
-unsigned long p0_t1= 0;
-unsigned long p1_t= 0;
-unsigned short rpm= 0;
-bool p0_flag = true;
-bool p1_flag = true;
+//CDI_DELAY_COUNTER
+//Require Arduino Version 1.6.23
+//CDI 脈衝點火時間差顯示器
 
-void p0_ISR() {
+//Author：
+//        田同學  2021/08/12 夜
+//        羽山 2021/08/13
+//接角說明
+//D_CDI 接 CDI 點火訊號<->高壓線圈之間 (D1)
+//D_INT 接 脈衝訊號 (凸台訊號) (D3) 
+//ESP8266 使用 D3
+//Arduino Uno使用D2
+//D8 TM1637 CLK
+//D7 TM1637 DIO
+
+#include <TM1637.h>
+//用來偵測 D1 腳抓脈衝凸台的訊號
+
+volatile unsigned long p0_t = 0;
+volatile unsigned long p0_t1= 0;
+volatile unsigned long fire_time= 0;
+volatile unsigned short rpm = 0;
+volatile bool p0_flag = true;
+volatile bool start_wait_fire = false;
+//volatile bool p1_flag = false;
+volatile unsigned long duty = 0;
+const int MY_D7 = D7;
+const int MY_D8 = D8;
+
+const int MY_D_INT = D3;
+const int MY_D_CDI = D1;
+
+#define CLK MY_D8
+#define DIO MY_D7
+
+ 
+TM1637 tm1637(CLK,DIO);
+
+
+void playFirstTime()
+{
+  // 0000~9999 跑二次
+  for(int i=0;i<= 9;i++)
+  {
+    for(int j=0;j<4;j++)
+    {
+      tm1637.display(j,i);    
+    }
+    delay(100);  
+  }
+}
+void diaplayOnLed(int show_rpm)
+{
+  //將show_rpm，變成顯示值
+  //例如 1500 轉 = 每分鐘  1500 轉，每秒 25   轉，1轉 = 0.04 秒 =  40.000 ms =  40000us = 顯示為 4000
+  //例如 6000 轉，                 每秒 60   轉，1轉 = 0.01666... 秒 =  16.667 ms = 16667us = 顯示為 1666
+  //例如 14000 轉 = 每分鐘 14000 轉，每秒 233.3 轉，1轉 = 0.0042863. 秒 =   4.286 ms =   4286us = 顯示為 428
+  //只顯示 萬千百十
+  //如果要顯示 千百十個，就不用除了
+  //太多數位有點眼花
+  String rpm_str = String(show_rpm/10);
+  if(rpm_str.length()<=3)
+  {
+    rpm_str = lpad(rpm_str,4,"X"); // 變成如 "XXX0"
+  }
+  //Serial.print("\nAfter lpad:");
+  //Serial.println(rpm_str);
+  for(int i=0;i<4;i++)
+  { 
+      if(rpm_str[i]=='X')
+      {
+        tm1637.display(i,-1); //-1 代表 blank 一片黑    
+      }
+      else
+      {
+        // Serial.println(rpm_str[i]);
+        // 腦包直接轉回 String 再把單字轉 int
+        // From : https://www.arduino.cc/en/Tutorial.StringToIntExample
+        tm1637.display(i,String(rpm_str[i]).toInt());    
+      }
+  }
+}
+String lpad(String temp , byte L , String theword){
+  //用來補LED左邊的空白
+  byte mylen = temp.length();
+  if(mylen > (L - 1))return temp.substring(0,L-1);
+  for (byte i=0; i< (L-mylen); i++) 
+    temp = theword + temp;
+  return temp;
+}
+void p0_ISR() {  
   if (p0_flag) {
     p0_t = micros();
+    start_wait_fire = true;
   }
   else {
     p0_t1 = micros() - p0_t;
@@ -16,25 +99,77 @@ void p0_ISR() {
   }
   p0_flag = !p0_flag;
 }
-void p1_ISR() {
+void setup() {
+  
+  Serial.begin(115200);
+  // 4 Digit-Display
+  tm1637.init();  
+  tm1637.set(BRIGHT_TYPICAL); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+  playFirstTime();
+  //playFirstTime();
+  //回歸零
+  //tm1637.display(0,-1); //-1 for blank
+  //tm1637.display(1,-1); //-1 for blank
+  //tm1637.display(2,-1); //-1 for blank
+  //tm1637.display(3,0);
+  diaplayOnLed(0);
+  
+  pinMode(MY_D_CDI,INPUT);
+  pinMode(MY_D_INT,INPUT);
+  
+  
+  attachInterrupt(digitalPinToInterrupt(MY_D_INT), p0_ISR, CHANGE); //接訊號 //偵測點火 (凸台) //脈衝訊號  
+  //下版再試
+  //attachInterrupt(digitalPinToInterrupt(MY_D_CDI), p0_ISR, RISING); //D3 
+  
+  Serial.println("");
+  Serial.println("Program Start:");
+  Serial.println(digitalPinToInterrupt(MY_D_INT));
+  //Serial.println(digitalPinToInterrupt(MY_D3));
+  //noInterrupts();
+  //interrupts();
+}
+
+void loop() {  
+  if (start_wait_fire) // digitalRead(MY_D_CDI)==HIGH) 
+  {
+    //p1_flag = false;
+    start_wait_fire = false;
+    //fire_time = micros();        
+    //duty
+    duty = fire_time - p0_t;    
+    Serial.print("RPM: ");
+    Serial.print(rpm);        
+    //Serial.print(" , ");
+    //Serial.print(fire_time);
+    //Serial.print(" - ");
+    //Serial.print(p0_t);
+    //Serial.print(" = ");
+    //Serial.print(duty);
+    Serial.println();
+    /*
+     * 參考：http://stm32-learning.blogspot.com/2014/05/arduino.html   
+     * 轉速  1500 轉 = 每分鐘  1500 轉，每秒  25   轉，1轉 = 0.04       秒 =  40.000 ms =  40000us
+     * 轉速  6000 轉 = 每分鐘  6000 轉，每秒  60   轉，1轉 = 0.01666... 秒 =  16.667 ms =  16667us
+     * 轉速 14000 轉 = 每分鐘 14000 轉，每秒 233.3 轉，1轉 = 0.0042863. 秒 =   4.286 ms =   4286us
+     * 轉速 16000 轉 = 每分鐘 16000 轉，每秒 266.6 轉，1轉 = 0.0037500. 秒 =   3.750 ms =   3750us
+    */             
+  }
+ 
+}
+/*
+  //讓 LED 不要刷的太快，太快眼睛跟不上字會黏在一起
+  //if (micros()%10000==0) 
+  //{       
+    //diaplayOnLed(duty);        
+  //}  
+  //digitalWrite(MY_D3,HIGH);
+  //t=!t;
+  //delay(1);
+ */
+
+/*void p1_ISR() {
   p1_t = micros();
   p1_flag = true;
 }
-void setup() {
-  pinMode(D3,INPUT);
-  pinMode(D4,INPUT);
-  attachInterrupt(0, &p0_ISR, RISING); //D3 接訊號
-  //attachInterrupt(digitalPinToInterrupt(D4), &p1_ISR, RISING); //D4 接CDI
-  Serial.begin(9600);
-}
-void loop() {
-  //noInterrupts();
-  //interrupts();
-  if (p1_flag) {
-    //p1_flag = false;
-    Serial.print(rpm);
-    Serial.println(" ");
-    //Serial.println(p1_t - p0_t); //duty
-  }
-  
-}
+*/
---
+++ test/test.ino
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+const int SensorPin = D3;  //Define Interrupt Pin (2 or 3 @ Arduino Uno)
+const int Purse_Pin = D6;  //產生訊號角
+//#define myPin D6
+//#define myPinBit (1<<myPin)
+volatile unsigned long C=micros(), rpm=0,RPM_DELAY=0, VR_RPM=100,duty_half=598802,isShowCount=0;
+volatile bool CFLAG=false;
+volatile unsigned int VR_LOW = 999;
+volatile unsigned int VR_HIGH = 0;
+volatile unsigned long test = 0;
+//From : https://forum.arduino.cc/t/arduino-esp8266-faster-direct-port-write/407251/3
+#define myPinBit (1<<15)
+void countup() {
+  if(CFLAG==false)
+  {
+    C=micros();
+    RPM_DELAY=0;
+    rpm = 99999;
+  }
+  else
+  {
+    RPM_DELAY=micros()-C;
+    rpm = 60000000UL / RPM_DELAY; 
+  }
+  CFLAG=!CFLAG;  
+}
+
+void setup(){
+  delay(1000);
+
+  Serial.begin(115200);
+  Serial.println("Counting...");  
+  pinMode(SensorPin, INPUT_PULLUP);
+  pinMode(Purse_Pin, OUTPUT);
+  //analogWriteFreq(22);
+  //int o = 512;//map(22,0,233,0,255);
+  //analogWriteResolution(13);
+  //analogWrite(D6,o);  
+  attachInterrupt(digitalPinToInterrupt(SensorPin), countup, RISING);  
+}
+void loop() {
+  //test = (((1*1000*1000)/(1500/60)) ) / 2.0;
+  //Serial.println(test);
+  //return;
+  //Serial.println(o);
+  uint16_t  VR = analogRead(A0);
+  VR = (VR > 1023)?1023:VR;
+  VR_LOW = (VR<VR_LOW)?VR:VR_LOW;
+  VR_HIGH = (VR>VR_HIGH)?VR:VR_HIGH;
+  
+  int HZ = 0;
+  if(VR_LOW!=999 && VR_HIGH!=0)
+  {
+    //自動修正VR上下限值
+    //HZ = map(VR,VR_LOW,VR_HIGH,1,250); //15000
+    HZ = map(VR,VR_LOW,VR_HIGH,1,500); //30000
+  }
+  else
+  {
+    //HZ = map(VR,9,1023,1,250); //初值 9~1023 , 15000rpm
+    HZ = map(VR,9,1023,1,500); //初值 30000rpm
+  }
+  //VR_RPM = map(HZ,1,250,0,15000); //0~15000RPM
+  VR_RPM = map(HZ,1,500,0,30000); //0~15000RPM
+  
+  /*
+  參考：http://stm32-learning.blogspot.com/2014/05/arduino.html
+  轉速 100 轉 = 每分鐘 100 轉，每秒 1.67 轉，1轉多少秒呢，一轉 = 0.598802 秒 = 598.802 ms = 598802us
+  轉速 200 轉 = 每分鐘 200 轉，每秒 3.3 轉，1轉多少秒呢，一轉 = 0.300003 秒 = 300.003 ms = 300003us
+  轉速 600 轉 = 每分鐘 600 轉，每秒 10 轉，1轉多少秒呢，一轉 = 0.1 秒 = 100.000 ms = 100000us
+  轉速 1000 轉 = 每分鐘 1000 轉，每秒 16.67 轉，1轉多少秒呢，一轉 = 0.059989 秒 = 59.989 ms = 59989us
+  轉速 1500 轉 = 每分鐘 1500 轉，每秒 25 轉，1轉多少秒呢，一轉 = 0.04 秒 = 40.000 ms = 40000us
+  轉速 6000 轉 = 每分鐘 6000 轉，每秒 60 轉，1轉多少秒呢，一轉 = 0.01 秒 = 10.000 ms = 10000us
+  轉速 12000 轉 = 每分鐘 12000 轉，每秒 120 轉，1轉多少秒呢，一轉 = 0.005 秒 = 5.000 ms = 5000us
+  轉速 14000 轉 = 每分鐘 14000 轉，每秒 233.3 轉，1轉多少秒呢，一轉 = 0.0042863 秒 = 4.286 ms = 4286us
+  轉速 15000 轉 = 每分鐘 15000 轉，每秒 250 轉，1轉多少秒呢，一轉 = 0.004 秒 = 4.000 ms = 4000us
+  轉速 16000 轉 = 每分鐘 16000 轉，每秒 266.6 轉，1轉多少秒呢，一轉 = 0.0037500 秒 = 3.750 ms = 3750us
+  轉速 20000 轉 = 每分鐘 20000 轉，每秒 333.333 轉，1轉多少秒呢，一轉 = 0.003000003 秒 = 3.000 ms = 3000us
+  轉速 30000 轉 = 每分鐘 30000 轉，每秒 500 轉，1轉多少秒呢，一轉 = 0.002 秒 = 2.000 ms = 2000us
+  */
+  //Serial.println(HZ);
+  //analogWriteFreq(HZ);
+  //analogWrite(D6,512); 
+    
+
+  //if(isShowCount % 500==0)
+  {
+    display_rpm();    
+    //isShowCount=0;
+  }
+
+  if(VR_RPM>35000)
+  {
+    return;
+  }
+  duty_half = ((1000000/(VR_RPM/60.0)) ) / 2.0; //一正一負 (us)
+  if(duty_half>500000) return; //WTF
+  //From : https://roboticsbackend.com/arduino-fast-digitalwrite/
+  
+  digitalWrite(Purse_Pin,HIGH); //一個 digitalWrite 會 delay 3.4us
+  //WRITE_PERI_REG( 0x60000304, myPinBit ); //on
+  delayMicroseconds(duty_half);
+  digitalWrite(Purse_Pin,LOW);
+  //WRITE_PERI_REG( 0x60000308, myPinBit ); //off
+  delayMicroseconds(duty_half);
+  //isShowCount++;
+}
+
+
+
+
+void display_rpm() {  
+  if(rpm > 30000 || VR_RPM > 30000) return;
+  Serial.print("VR_LOW: ");
+  Serial.print(VR_LOW);
+  Serial.print(" , VR_HIGH: ");
+  Serial.print(VR_HIGH);
+  Serial.print(" , RPM: ");
+  Serial.print(rpm);
+  Serial.print(" , VR_RPM: ");  
+  Serial.print(VR_RPM);
+  Serial.print(" , duty_half: ");  
+  Serial.println(duty_half);
+  
+}
