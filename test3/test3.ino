a0b6142f79d74c26f798ef4b70a4d0c79a6dd1e2
羽山 <linainverseshadow@gmail.com>
2021-08-21 17:26:02 +0800

    upload

--- test3/test3.ino
+++ test3/test3.ino
@@ -5,7 +5,8 @@ const int ToPin = D3;  //凸台
 const int FirePin = D1;  //點火
 //0~14000
 const int degree[15] = {12,12,12,23,29,27,25,23,20,17,13,10,8,8,8}; 
-const int fullAdv = 32;
+
+const int fullAdv = 29;
 #define CLK D5 
 #define DIO D4 
 TM1637 tm1637(CLK,DIO);
@@ -30,9 +31,18 @@ void ICACHE_RAM_ATTR countup() {  //For newest version
       index = (index>=15)? 14:index;
       index = (index<=0) ?  1:index;
       int r = degree[index];
-      CDI_DELAY =(int)((1000000)/((index*1000)/60.0))*((fullAdv-r)/360.0);
-      display_rpm();    
+      CDI_DELAY =(int)((1000000)/((index*1000)/60.0))*((fullAdv-r)/360.0);      
+      //CDI_DELAY =(int)((1000000)/((index*1000)/60.0))*((r)/360.0);      
+      //display_rpm();    
+      delayMicroseconds(CDI_DELAY);
+      //diaplayOnLed(r);
+      //rpm=0;
       digitalWrite(FirePin,HIGH);
+      //點火持續時間
+      //int fireTimes = (int)(map(rpm,600,14000,350,80));
+      delayMicroseconds(350);
+      digitalWrite(FirePin,LOW);
+      
     }
           
   }
@@ -46,6 +56,43 @@ void ICACHE_RAM_ATTR countup() {  //For newest version
   CFLAG=!CFLAG;    
 }
 
+void diaplayOnLed(int show_rpm)
+{
+  //將轉速，變成顯示值
+  //只顯示 萬千百十
+  //如果要顯示 千百十個，就不用除了
+  //太多數位有點眼花
+  //String rpm_str = String(show_rpm/10);
+  String rpm_str = String(show_rpm);
+  if(rpm_str.length()<=3)
+  {
+    rpm_str = lpad(rpm_str,4,"X"); // 變成如 "XXX0"
+  }
+  //Serial.print("\nAfter lpad:");
+  //Serial.println(rpm_str);
+  for(int i=0;i<4;i++)
+  { 
+      if(rpm_str[i]=='X')
+      {
+        tm1637.display(i,-1); //-1 代表 blank 一片黑    
+      }
+      else
+      {
+        // Serial.println(rpm_str[i]);
+        // 腦包直接轉回 String 再把單字轉 int
+        // From : https://www.arduino.cc/en/Tutorial.StringToIntExample
+        tm1637.display(i,String(rpm_str[i]).toInt());    
+      }
+  }
+}
+String lpad(String temp , byte L , String theword){
+  //用來補LED左邊的空白
+  byte mylen = temp.length();
+  if(mylen > (L - 1))return temp.substring(0,L-1);
+  for (byte i=0; i< (L-mylen); i++) 
+    temp = theword + temp;
+  return temp;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -54,9 +101,14 @@ void setup() {
   pinMode(ToPin, INPUT_PULLUP);
   pinMode(FirePin, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(ToPin), countup, RISING);   
+  tm1637.init();  
+  tm1637.set(BRIGHT_TYPICAL); //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+  playFirstTime();
+  diaplayOnLed(0);
 }
 
 void loop() {
+  
   // put your main code here, to run repeatedly:
   //if(isShowCount > 100==0 )
   {
@@ -64,6 +116,18 @@ void loop() {
   //  isShowCount=0;
   }
   //isShowCount++;
+}
+void playFirstTime()
+{
+  // 0000~9999 跑二次
+  for(int i=0;i<= 9;i++)
+  {
+    for(int j=0;j<4;j++)
+    {
+      tm1637.display(j,i);    
+    }
+    delay(100);  
+  }
 }
 void display_rpm() {  
   if(rpm > 30000) return;
